d7ca4b61d8878cbfa38fb136a3c3eed925ed03c3
Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>
2024-07-12 13:56:24 +0200

    feat: add `target_os`-dependent autoconfiguration
    Signed-off-by: Martin Kröning <martin.kroening@eonerc.rwth-aachen.de>

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,10 @@
 //! # Features
 //!
 //! This crate has the following Cargo features:
-//! - `linux` (default) enables the [`linux`] module.
+//! - `linux` enables the [`linux`] module.
+//!   - Automatically enabled on Linux systems.
 //! - `macos` enables the [`macos`] module.
+//!   - Automatically enabled on macOS systems.
 //! - `num_enum` derives the following traits for enums:
 //!   - [`num_enum::IntoPrimitive`]
 //!   - [`num_enum::TryFromPrimitive`]
@@ -22,11 +24,21 @@
 ///
 /// [`fuse(4)`]: https://www.man7.org/linux/man-pages/man4/fuse.4.html
 /// [`linux/fuse.h`]: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/include/uapi/linux/fuse.h?h=v6.9
-#[cfg(feature = "linux")]
+#[cfg(any(target_os = "linux", feature = "linux"))]
 pub mod linux;
 
 /// macFUSE device interface ([`fuse_kernel.h`]).
 ///
 /// [`fuse_kernel.h`]: https://github.com/osxfuse/fuse/blob/6f7322893456f6ff9db145f096b9bfc2ba95d627/include/fuse_kernel.h
-#[cfg(feature = "macos")]
+#[cfg(any(target_os = "macos", feature = "macos"))]
 pub mod macos;
+
+#[cfg(any(target_os = "linux", target_os = "macos"))]
+pub mod os {
+    //! OS-specific FUSE device interface.
+
+    #[cfg(target_os = "linux")]
+    pub use super::linux::*;
+    #[cfg(target_os = "macos")]
+    pub use super::macos::*;
+}
